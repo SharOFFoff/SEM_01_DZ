5221a7fe36174b0b39a5e5363f6435f777c1017d
SharOFF <sharov-pa@yandex.ru>
2022-11-23 13:37:12 +0300

    Task 01 выполнено

---
+++ Task_01/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число 1: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (n > m)
+    Console.Write( $" max = {n} ");
+if (n < m)
+    Console.Write( $" max = {m} ");
+if (n == m)
+    Console.Write( $"{n} = {m} ");
